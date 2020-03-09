04bd777e88891ded509c56eef8719a8d4a0dc29d
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2020-03-09 18:15:08 +0000

    Added some attributes to model classes

--- src/main/java/com/chibana/tasklist/model/Person.java
+++ src/main/java/com/chibana/tasklist/model/Person.java
@@ -1,8 +1,15 @@
 package com.chibana.tasklist.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Rodrigo Chibana
@@ -10,5 +17,19 @@ import javax.persistence.Entity;
  **/
 @Entity
 @Data
+@EqualsAndHashCode
 public final class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String secondName;
+
 }
--- src/main/java/com/chibana/tasklist/model/Task.java
+++ src/main/java/com/chibana/tasklist/model/Task.java
@@ -3,6 +3,12 @@ package com.chibana.tasklist.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Rodrigo Chibana
@@ -11,5 +17,20 @@ import javax.persistence.Entity;
 @Entity
 @Data
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    @ManyToOne
+    private Person assignedTo;
 
 }
